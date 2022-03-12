9704d233415d0bdd95188d3b3de946b926e0d372
Eric Rydberg <eric.rydberg@outlook.com>
2022-03-12 15:07:34 -0600

    added media player stub and image caching

---
+++ Downgrooves.Mobile/ViewModels/MediaPlayerViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Prism.Navigation;
+
+namespace Downgrooves.Mobile.ViewModels
+{
+    public class MediaPlayerViewModel : ViewModelBase
+    {
+        public MediaPlayerViewModel(INavigationService navigationService) : base(navigationService)
+        {
+        }
+    }
+}

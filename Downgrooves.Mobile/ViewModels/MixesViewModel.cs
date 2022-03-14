8fc5a0ba19f1af40a05b5519b076ff61fcc3124d
Eric Rydberg <eric.rydberg@outlook.com>
2022-03-14 15:59:47 -0500

    moved mixes list into a separate control

--- Downgrooves.Mobile/ViewModels/MixesViewModel.cs
+++ Downgrooves.Mobile/ViewModels/MixesViewModel.cs
@@ -57,6 +57,7 @@ namespace Downgrooves.Mobile.ViewModels
         {
             try
             {
+                Mixes.Clear();
                 LoadMixes();
             }
             catch (Exception ex)

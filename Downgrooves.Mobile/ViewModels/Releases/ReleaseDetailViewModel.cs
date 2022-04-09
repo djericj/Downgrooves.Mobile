305be1974d663b3ff48e3fd4cabe858bf20b48a2
Eric Rydberg <eric.rydberg@outlook.com>
2022-04-08 21:27:35 -0500

    added releases

---
+++ Downgrooves.Mobile/ViewModels/Releases/ReleaseDetailViewModel.cs
@@ -0,0 +1,47 @@
+﻿using Downgrooves.Mobile.Services.Interfaces;
+using Prism.Commands;
+using Prism.Navigation;
+using System.Windows.Input;
+
+namespace Downgrooves.Mobile.ViewModels.Releases
+{
+    public class ReleaseDetailViewModel : ViewModelBase
+    {
+        private ReleaseViewModel _release;
+
+        public ICommand OpenLinkCommand => new DelegateCommand<string>(OpenLink);
+
+        public ReleaseViewModel Release
+        {
+            get => _release;
+            set
+            {
+                _release = value;
+                RaisePropertyChanged(nameof(Release));
+            }
+        }
+
+        private string _previewIcon;
+
+        public string PreviewIcon
+        {
+            get { return _previewIcon; }
+            set { SetProperty(ref _previewIcon, value); }
+        }
+
+        public ReleaseDetailViewModel(INavigationService navigationService) : base(navigationService)
+        {
+        }
+
+        public override void OnNavigatedTo(INavigationParameters parameters)
+        {
+            base.OnNavigatedTo(parameters);
+
+            PreviewIcon = "resource://Downgrooves.Mobile.Resources.play.svg";
+
+            if (_release != null) return; // release is already loaded.
+
+            Release = parameters["release"] as ReleaseViewModel;
+        }
+    }
+}
---
+++ Downgrooves.Mobile/ViewModels/Releases/ReleasesViewModel.cs
@@ -0,0 +1,152 @@
+﻿using Downgrooves.Mobile.Services.Interfaces;
+using Prism.Commands;
+using Prism.Navigation;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows.Input;
+using Xamarin.CommunityToolkit.ObjectModel;
+
+namespace Downgrooves.Mobile.ViewModels.Releases
+{
+    public class ReleasesViewModel : ViewModelBase, INavigationAware
+    {
+        private int _pageSize = App.Settings.MixSettings.PageSize;
+        private int _pageNumber = 0;
+        private readonly IReleaseService _releaseService;
+        private int _artistId = 0;
+        private bool _isOriginal = false;
+        private bool _isRemix = false;
+
+        public ICommand LoadReleasesCommand => new DelegateCommand(LoadMore);
+
+        public ICommand RefreshCommand => new DelegateCommand(Refresh);
+
+        public ICommand NavigateToReleaseCommand => new DelegateCommand<ReleaseViewModel>(NavigateToRelease);
+
+        private ObservableRangeCollection<ReleaseViewModel> _releases;
+
+        public ObservableRangeCollection<ReleaseViewModel> Releases
+        {
+            get { return _releases; }
+            set { SetProperty(ref _releases, value); }
+        }
+
+        private int _itemThreshold;
+
+        public int ItemThreshold
+        {
+            get { return _itemThreshold; }
+            set { SetProperty(ref _itemThreshold, value); }
+        }
+
+        private bool _isBusy;
+
+        public bool IsBusy
+        {
+            get { return _isBusy; }
+            set { SetProperty(ref _isBusy, value); Debug.WriteLine($"IsBusy: {IsBusy}"); }
+        }
+
+        private bool _isRefreshing;
+
+        public bool IsRefreshing
+        {
+            get { return _isRefreshing; }
+            set { SetProperty(ref _isRefreshing, value); Debug.WriteLine($"IsRefreshing: {IsRefreshing}"); }
+        }
+
+        public ReleasesViewModel(INavigationService navigationService, IReleaseService releaseService) : base(navigationService)
+        {
+            _releaseService = releaseService;
+        }
+
+        public void Refresh()
+        {
+            try
+            {
+                Releases.Clear();
+                LoadReleases();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+            finally
+            {
+                IsRefreshing = false;
+            }
+        }
+
+        public void LoadMore()
+        {
+            _pageNumber++;
+            LoadReleases(_pageNumber);
+        }
+
+        public void LoadReleases()
+        {
+            _pageNumber = 1;
+            ItemThreshold = App.Settings.MixSettings.ItemThreshold;
+            Releases = new ObservableRangeCollection<ReleaseViewModel>();
+            LoadReleases(_pageNumber);
+        }
+
+        public async void LoadReleases(int pageNumber)
+        {
+            if (IsBusy) return;
+
+            Debug.WriteLine($"Load More Page Number: {pageNumber}");
+
+            IsBusy = true;
+
+            try
+            {
+                var releasesList = await _releaseService.GetReleases(pageNumber, _pageSize,
+                    artistId: _artistId,
+                    isOriginal: _isOriginal,
+                    isRemix: _isRemix);
+                releasesList = releasesList.OrderByDescending(x => x.ReleaseDate);
+
+                if (releasesList.Any())
+                {
+                    Releases.AddRange(new ObservableRangeCollection<ReleaseViewModel>(releasesList));
+                    Debug.WriteLine($"{releasesList.Count()} {Releases.Count} ");
+                }
+                else
+                {
+                    ItemThreshold = -1;
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        public override void OnNavigatedTo(INavigationParameters parameters)
+        {
+            _artistId = Convert.ToInt32(parameters["ArtistId"]);
+            _isOriginal = Convert.ToBoolean(parameters["isOriginal"]);
+            _isRemix = Convert.ToBoolean(parameters["isRemix"]);
+
+            Title = parameters["Title"].ToString();
+
+            LoadReleases();
+        }
+
+        private async void NavigateToRelease(ReleaseViewModel release)
+        {
+            var props = new NavigationParameters()
+            {
+                {"release",  release}
+            };
+            await NavigationService.NavigateAsync("ReleaseDetail", props);
+        }
+    }
+}
